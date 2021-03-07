06e1be7c441cc0e9093655b69c9d8d54feb5c00b
MightyK <KMightyDev@gmail.com>
2021-03-06 21:41:30 -0500

    Initial commit using passport.js to add cookies/sessions

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-require('dotenv').config();
+require('dotenv').config(); // Requires and activates .env and its configurations
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
+
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+
+// const md5 = require("md5"); // Hash function to turn strings into irreversible hash.
+
+
+// const encrypt = require("mongoose-encryption"); // Requires the mongoose encryption dictionary
 
 const app = express();
 
@@ -13,7 +25,33 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({
+    secret: "The highest truths are right in front of us.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:127.0.0.1:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get('/', function(req, res) {
     res.render("home");
@@ -27,53 +65,60 @@ app.get('/register', function(req, res) {
     res.render('register');
 });
 
-
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+app.get('/secrets', function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
+    }
 });
 
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
-
-const User = new mongoose.model("User", userSchema);
+// Adds the specified encryption method to the schema and encrypts using the secret key stored in the env file //
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 app.post('/register', function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    const newUser = new User({
-        email: username,
-        password: password
-    });
-
-    newUser.save(function(err) {
+    User.register({username: username}, password, function(err, user) {
         if (err) {
             console.log(err);
+            res.redirect("/register");
         } else {
-            res.render('secrets');
-            console.log(`Account for ${newUser.email} created successfully!`);
+            passport.authenticate("local")(req, res, function() {
+                res.redirect('/secrets');
+            });
         }
     });
+
+
 });
 
 app.post('/login', function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    User.findOne({email: username}, function(err, foundUser) {
+    const user = new User({
+        username: username,
+        password: password
+    });
+
+    req.login(user, function(err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render('secrets');
-                }
-            }
+            passport.authenticate("local")(req, res, function() {
+                res.redirect('/secrets');
+            });
         }
     });
+
 });
 
 
